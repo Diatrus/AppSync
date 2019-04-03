546bce03fb1a5c77b2c68384d5ca31f07afdbed9
Diatrus <diatrus@MacBook-Pro-4.local>
2019-04-03 01:53:53 -0400

    boom, fixed reboot issue

--- Makefile
+++ Makefile
@@ -1,21 +1,19 @@
-ARCHS = arm64
-TARGET = appletv
-export GO_EASY_ON_ME=1
-export SDKVERSION=10.1
-TARGET = appletv:clang:10.1:10.1
-SYSROOT = $(THEOS)/sdks/AppleTVOS10.1.sdk
-DEBUG = 0
+export ARCHS = arm64
+export TARGET = appletv:clang:10.1:10.1
+# export GO_EASY_ON_ME = 1
+# export SDKVERSION = 10.1
+export SYSROOT = $(THEOS)/sdks/AppleTVOS10.1.sdk
+export FINALPACKAGE=1
 
-THEOS_PACKAGE_DIR_NAME = debs
-PACKAGE_VERSION = $(THEOS_PACKAGE_BASE_VERSION)
+# THEOS_PACKAGE_DIR_NAME = debs
+# PACKAGE_VERSION = $(THEOS_PACKAGE_BASE_VERSION)
 
 include $(THEOS)/makefiles/common.mk
 
 SUBPROJECTS += AppSyncUnified
 SUBPROJECTS += postinst
 SUBPROJECTS += preinst
-SUBPROJECTS += asu_inject
 include $(THEOS_MAKE_PATH)/aggregate.mk
 
 clean::
-	@rm -f debs/*.deb
+	@rm -f packages/*.deb
--- postinst/postinst.c
+++ postinst/postinst.c
@@ -10,7 +10,6 @@
 #define INSTALLD_PLIST_PATH_L L_LAUNCHDAEMON_PATH "/com.apple.mobile.installd.plist"
 #define INSTALLD_PLIST_PATH_SL SL_LAUNCHDAEMON_PATH "/com.apple.mobile.installd.plist"
 
-#define ASU_INJECT_PLIST_PATH L_LAUNCHDAEMON_PATH "/net.angelxwind.asu_inject.plist"
 
 static int run_launchctl(const char *path, const char *cmd) {
     const char *args[] = {"/bin/launchctl", cmd, path, NULL};
@@ -26,7 +25,7 @@ int main(int argc, const char **argv) {
     printf("Copyright (C) 2014-2019 Linus Yang, Karen (angelXwind), Diatrus\n");
     printf("** PLEASE DO NOT USE APPSYNC UNIFIED FOR PIRACY **\n");
     if (access(DPKG_PATH, F_OK) == -1) {
-        printf("You seem to have installed AppSync Unified from an APT repository that is not diatrus.github.io/appsync (package ID us.diatr.appsyncunified).\n");
+        printf("You seem to have installed AppSync Unified from an APT repository that is not diatr.us/appsync (package ID us.diatr.appsyncunified).\n");
         printf("If someone other than Linus Yang (laokongzi), Karen (angelXwind), or Diatrus is taking credit for the development of this tool, they are likely lying.\n");
         printf("Please only download AppSync Unified from the official repository to ensure file integrity and reliability.\n");
     }
@@ -36,13 +35,6 @@ int main(int argc, const char **argv) {
     }
     run_launchctl(INSTALLD_PLIST_PATH_SL, "unload");
     run_launchctl(INSTALLD_PLIST_PATH_SL, "load");
-    printf("Current device is an Apple TV, enabling asu_inject...\n");
-    chown(ASU_INJECT_PLIST_PATH, 0, 0);
-    chmod(ASU_INJECT_PLIST_PATH, 0644);
-    run_launchctl(ASU_INJECT_PLIST_PATH, "unload");
-    #ifdef POSTINST
-        run_launchctl(ASU_INJECT_PLIST_PATH, "load");
-    #endif
     printf("finish:restart");
     return 0;
 }
