22aa71568f84f55c63beb5ca301a6e3b05b393b7
Diatrus <pnseeker75@outlook.com>
2020-01-01 18:49:47 -0500

    updates from origin

--- postinst/postinst.c
+++ postinst/postinst.c
@@ -13,7 +13,7 @@
 #define INSTALLD_PLIST_PATH_SL SL_LAUNCHDAEMON_PATH "/com.apple.mobile.installd.plist"
 
 static int run_launchctl(const char *path, const char *cmd) {
-    const char *args[] = {"/bin/launchctl", cmd, path, NULL};
+    const char *args[] = {(access("/sbin/launchctl", X_OK) != -1) ? "/sbin/launchctl" : "/bin/launchctl", cmd, path, NULL};
     pid_t pid;
     int stat;
     posix_spawn(&pid, args[0], NULL, NULL, (char **) args, NULL);
